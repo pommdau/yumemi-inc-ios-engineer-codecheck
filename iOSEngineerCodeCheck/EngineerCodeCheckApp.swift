65050ec2e3626f0ae26b9c1ca2faf19ad999ee79
IKEH1024 <pondau1024@gmail.com>
2022-11-12 16:35:20 +0900

    refactor: 起動時の画面をStoryboardからSwiftUIに置き換え(#14)
    ただし現状表示しているのはデモ画面となっている

---
+++ iOSEngineerCodeCheck/EngineerCodeCheckApp.swift
@@ -0,0 +1,18 @@
+//
+//  EngineerCodeCheckApp.swift
+//  iOSEngineerCodeCheck
+//
+//  Created by HIROKI IKEUCHI on 2022/11/12.
+//  Copyright © 2022 YUMEMI Inc. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct EngineerCodeCheckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
