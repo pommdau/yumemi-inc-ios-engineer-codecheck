b145a6422558b5ddc69cd8b84a1544b2eb2c6341
IKEH1024 <pondau1024@gmail.com>
2024-09-25 13:02:26 +0900

    settings: Danger-Swiftを利用してSwiftLintを実行するように変更

---
+++ Danger.swift
@@ -0,0 +1,14 @@
+//
+//  Danger.swift
+//  iOSEngineerCodeCheck
+//
+//  Created by HIROKI IKEUCHI on 2024/09/25.
+//  Copyright © 2024 YUMEMI Inc. All rights reserved.
+//
+
+import Danger
+
+let danger = Danger()
+
+// SwiftLintの実行結果をインラインで表示
+SwiftLint.lint(inline: true)
