1e7d3d76081680a1abfcc0522f547ea1679c100f
IKEH1024 <pondau1024@gmail.com>
2024-09-25 13:16:43 +0900

    wip

--- Danger.swift
+++ Danger.swift
@@ -12,3 +12,14 @@ let danger = Danger()
 
 // SwiftLintの実行結果をインラインで表示
 SwiftLint.lint(.modifiedAndCreatedFiles(directory: nil), inline: true)
+
+// SwiftLintの結果を処理して、warning または error の場合にPRに表示
+for violation in swiftLintViolations where violation.severity == .warning || violation.severity == .error {
+    let message = "SwiftLint \(violation.severity.rawValue.capitalized): \(violation.reason) in \(violation.file):\(violation.line ?? 0)"
+    
+    if violation.severity == .warning {
+        warning(message)
+    } else if violation.severity == .error {
+        fail(message)
+    }
+}
